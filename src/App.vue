d0d7ecc0c52efdad2fab0e481cffee9b05b9fb8e
John Doe <jd@mail.web>
2023-10-26 14:41:44 +0300

    completed

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ProductDetails v-for="p in products"
+                  :key="p.name"
+                  :productName="p.name"
+                  :price="p.price"
+                  :available="p.available">
+  </ProductDetails>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductDetails from './components/ProductDetails.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ProductDetails
+  },
+  data() {
+    return {
+      products: [
+        {
+          name: "Product 1",
+          price: 25.75,
+          available: true
+        },
+        {
+          name: "Product 2",
+          price: 50.25,
+          available: false
+        },
+        {
+          name: "Product 3",
+          price: 15.99,
+          available: true
+        }
+      ]
+    };
+  },
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 200px;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 }
 </style>
---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="product-details">
+        <div class="product-details__name">
+            {{ productName }}
+        </div>
+        <div class="product-details__price">
+            {{ formattedPrice }}
+        </div>
+        <div class="product-details__available" :class="{ 'product-details__available_yes': available }">
+            {{ availableText }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        productName: String,
+        price: Number,
+        available: Boolean
+    },
+    computed: {
+        formattedPrice() {
+            return '$' + this.price.toPrecision(4);
+        },
+        availableText() {
+            return this.available ? "Available" : "Out of stock";
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+.product-details {
+    width: 100%;
+    border: 2px solid gray;
+    background-color:darkgray;
+    padding-top: 30px;
+    padding-bottom: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
+
+    &__name {
+        font-size: large;
+    }
+
+    &__price {
+        font-size: larger;
+        color: gold;
+    }
+
+    &__available {
+        font-size: medium;
+        color: red;
+
+        &_yes {
+            color: green;
+        }
+    }
+}
+</style>
